23d92a07854e2c5f5cbfe2dd8fc7bc6586c9744d
Ri-Dearg <sheridan.rp@gmail.com>
2020-03-13 18:36:16 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello WOrld'
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
