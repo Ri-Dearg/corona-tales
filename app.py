ef27785954c9daa6603b58b46dd680d1f3571713
Ri-Dearg <sheridan.rp@gmail.com>
2020-04-11 18:22:56 +0000

    Added basic form to insert document into database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -14,6 +14,13 @@ mongo = PyMongo(app)
 @app.route('/')
 def story_page():
     return render_template('index.html', stories=mongo.db.stories.find())
+
+
+@app.route('/create_story', methods=['POST'])
+def create_story():
+    stories = mongo.db.stories
+    stories.insert_one(request.form.to_dict())
+    return redirect(url_for('story_page'))
 
 
 if __name__ == '__main__':
--- static/js/init.js
+++ static/js/init.js
@@ -1,7 +1,9 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-
-  }); // end of document ready
+(function ($) {
+    M.AutoInit();// end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {});
+
+});
