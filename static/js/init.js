5a73a10f0091a1efe55653e8db3db167ddb7c75b
Ri-Dearg <sheridan.rp@gmail.com>
2020-04-15 14:45:37 +0000

    Added CKEditor to textarea input

--- static/js/init.js
+++ static/js/init.js
@@ -1,9 +1,3 @@
 (function ($) {
     M.AutoInit();// end of document ready
 })(jQuery); // end of jQuery name space
-
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {});
-
-});
