eebbca86ab99bd0716657f171917c36ffabf2d6d
Ri-Dearg <sheridan.rp@gmail.com>
2020-04-16 14:43:41 +0000

    Added code for tagging system

--- static/js/script.js
+++ static/js/script.js
@@ -5,39 +5,31 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 function initTags(tagCreate) {
-    var chips = document.querySelectorAll('.chips');
-    var instances = M.Chips.init(chips, {
-    limit: 15,
-    placeholder: 'Enter a Tag', 
-    secondaryPlaceholder: '+Tag',
-    onChipAdd: function() {
-        console.log(M.Chips.getInstance($('.chips')).chipsData[0].tag)
-        console.log(Object.keys((M.Chips.getInstance($('.chips')).chipsData)).length)
-    },
-    autocompleteOptions: {
-        data: tagCreate,
-        limit: 8,
-    }
+    document.addEventListener('DOMContentLoaded', function() {
+        var chips = document.querySelectorAll('.chips');
+        
+        var instances = M.Chips.init(chips, {
+            limit: 15,
+            placeholder: 'Enter a Tag', 
+            secondaryPlaceholder: '+Tag',
+            autocompleteOptions: {
+                data: tagCreate,
+                limit: 8,
+            }
+        });
 
-});
-}
-
-/*
-    var tagDict = M.Chips.getInstance($('.chips')).chipsData
-    var tagDictLength = Object.keys((M.Chips.getInstance($('.chips')).chipsData)).length
-
-    function addTags(length, array) {
-        for (i=0; i < length; i++) {
-            console.log(array[i].tag);
-            $("#create-story").append(`<input type="hidden" name="tags" value="${array[i].tag}">`);
-            return true;
+        function addTags(length, array) {
+            for (i=0; i < length; i++) {
+                console.log(array[i].tag);
+                $("#create-story").append(`<input type="hidden" name="tags" value="${array[i].tag}">`);
+            }
         }
-    }
+        
+        $("#create-story").on('submit', function(event) {
+            var tagDict = M.Chips.getInstance($('.chips')).chipsData
+            var tagDictLength = Object.keys((M.Chips.getInstance($('.chips')).chipsData)).length
 
-
-    $("#post-story").on('submit', function(event) {
-        addTags(tagDictLength, tagDict)
-        event.preventDefault
+            addTags(tagDictLength, tagDict)
     });
-});
-*/
+    })
+};
